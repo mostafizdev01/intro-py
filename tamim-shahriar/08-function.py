4c6c1c430ad642762050df136318567ce36e4bdc
mostafiz <mostafiz4372@gmail.com>
2026-07-15 00:18:16 +0600

    print function and loop

---
+++ tamim-shahriar/08-function.py
@@ -0,0 +1,26 @@
+
+def multiple_with_function(n):
+    print(n, "x 1 =", n * 1)
+    print(n, "x 2 =", n * 2)
+    print(n, "x 3 =", n * 3)
+    print(n, "x 4 =", n * 4)
+    print(n, "x 5 =", n * 5)
+    print(n, "x 6 =", n * 6)
+    print(n, "x 7 =", n * 7)
+    print(n, "x 8 =", n * 8)
+    print(n, "x 9 =", n * 9)
+    print(n, "x 10 =", n * 10)
+
+n = input("Enter your number: ")
+n = int(n)
+
+multiple_with_function(n)
+multiple_with_function(n+1)
+
+
+# library function
+
+import datetime
+
+today = datetime.datetime.today()
+print(today)

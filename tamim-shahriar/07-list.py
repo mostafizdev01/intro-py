3a0bcf7f4e7796cca9c15460e823a7f6000fdb61
mostafiz <mostafiz4372@gmail.com>
2026-07-14 22:32:01 +0600

    print list

---
+++ tamim-shahriar/07-list.py
@@ -0,0 +1,11 @@
+fruits = ["apple", "banana", "orange", "mango"] # create a list
+
+print(fruits) 
+
+print(fruits[0]) # print first items into list
+
+fruits.append("papaya") # add new item for fruits list
+fruits.pop() # remove last item for fruits list
+
+print(fruits)
+

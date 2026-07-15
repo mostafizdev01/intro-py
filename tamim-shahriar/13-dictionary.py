3009410878ef9a05c8abd3692689219a5aab9916
mostafiz <mostafiz4372@gmail.com>
2026-07-16 00:07:38 +0600

    print break-continue statement

--- tamim-shahriar/13-dictionary.py
+++ tamim-shahriar/13-dictionary.py
@@ -1,4 +1,8 @@
 num_to_words = {1: "one", 2: "two", 3: "three", 4: "four", 5: "five"}
 
+
 print(num_to_words.values())
 
+num_to_words.clear()
+
+print(num_to_words)
---
+++ tamim-shahriar/14-break-continue.py
@@ -0,0 +1,7 @@
+while True:
+    s = input("Do you want next process y/n: ")
+    
+    print(s.upper())
+    if s == "y": 
+        continue
+    break

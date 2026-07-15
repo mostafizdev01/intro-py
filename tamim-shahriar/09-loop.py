ec79efdcda78b5dde8f85048b1aa8272d4c7e5f0
mostafiz <mostafiz4372@gmail.com>
2026-07-15 15:46:24 +0600

    print arrow list and tuple

--- tamim-shahriar/09-loop.py
+++ tamim-shahriar/09-loop.py
@@ -1,4 +1,4 @@
-
+"""
 def multiple_with_function(n):
     print(n, "x 1 =", n * 1)
     print(n, "x 2 =", n * 2)
@@ -17,3 +17,27 @@ n = int(n)
 for i in range(1, n+1):
     multiple_with_function(i)
     print("")
+   """
+    
+    # while_loop for py
+    
+i = 1
+    
+while i <= 10:
+        print(i)
+        i = i + 1
+        
+# calculate multplication with while loop
+
+def multplication_table(n):
+    for i in range(1, 11):
+        print(" {} X {} = {}".format(n, i, n * i))
+        
+n = input("Enter a number (0 to exit) :")
+n = int(n)
+
+while n != 0:
+    multplication_table(n)
+    print("")
+    n = input("Enter a number (0 to exit): ")
+    n = int(n)
---
+++ tamim-shahriar/10-arrow-list.py
@@ -0,0 +1,28 @@
+# list.extend(list2)
+# li = [1, 2, 3, 4, 5, 6]
+# li2 = [7, 8, 9, 10]
+
+# li.extend(li2)
+# print(li)
+
+# list.insert(index, elem)
+# li = [1, 2, 3, 4, 5, 6]
+
+# li.insert(2, 9)
+# print(li)
+# li.sort()
+# print(li)
+
+# li.reverse()
+# print(li) 
+
+# list.index(elem)
+
+# list.remove(elem)
+
+# list.pop(index)
+
+# list.sort()
+
+# list.revers()
+

fe180b6a47fe019cf89cea4063cbbd1a36b98690
mostafiz <mostafiz4372@gmail.com>
2026-07-16 13:32:35 +0600

    print linear search

---
+++ tamim-shahriar/16-linear-search.py
@@ -0,0 +1,14 @@
+# Linear search
+
+li = [1, 3, 6, 10, 100, 14, 5, 9]
+key = 60
+found = False
+
+for item in li: 
+    found = True
+    break
+
+if found is True:
+    print("Found")
+else:
+    print("Not found")

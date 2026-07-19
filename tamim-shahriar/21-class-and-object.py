ca7aeb4a146f414b7d1b20d0c8b3c6d97568286b
mostafiz <mostafiz4372@gmail.com>
2026-07-19 09:44:18 +0600

    create class and object

---
+++ tamim-shahriar/21-class-and-object.py
@@ -0,0 +1,17 @@
+class Car:
+        
+        def ___init__(self, mk, mdl, yr):
+            self.make = mk
+            self.make = mdl
+            self.year = yr
+            
+            def move(self):
+                print("Car is moving")
+                
+            def horn(self):
+                print("Beep beep!")
+                
+mycar = Car("Subaru", "Forester", 2014)
+anohter_car = Car("Toyota", "Camry", 2020)
+        
+print(mycar, anohter_car)
